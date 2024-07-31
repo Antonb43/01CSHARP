c77cdb8a00d6a1d8d415368094fa2a51a6035602
AntonC <antonchernyshevv93@gmail.com>
2024-07-31 14:08:10 +0300

    Add lesson2:task3

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// int n = 10;
+// int[] array = { 11, 12, 87, 14, 121, 115, 123, 17, 19, 22 };
+// int i = 0;
+
+// while(i < n)
+// {
+//     if(array[i] % 2 == 0)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     i = i + 1;
+// }
+
+// int n = 10;
+int[] array = { 11, 12, 87, 14, 121, 115, 123, 17, 19, 22 };
+int i = 0;
+
+while(i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write(array[i]);
+        Console.Write(' ');
+    }
+    i = i + 1;
+}
