6bcd0f1f76d732fb64f9515b0e9c4ae528c1db92
AntonC <antonchernyshevv93@gmail.com>
2024-08-04 12:22:21 +0300

    add lesson3:task1

---
+++ Lesson4/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numentator = a * b;
+    int denumetatot = c + d;
+    double result = numentator / denumetatot;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
